4f37526478d7cf80597163fb8e5728b50a977fd9
Cocowwy <514658459@qq.com>
2022-08-30 14:19:19 +0800

    init project

---
+++ happypush/src/main/java/cn/monksfish/happypush/HappypushApplication.java
@@ -0,0 +1,13 @@
+package cn.monksfish.happypush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappypushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappypushApplication.class, args);
+	}
+
+}
---
+++ happypush/src/test/java/cn/monksfish/happypush/HappypushApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.monksfish.happypush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappypushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
